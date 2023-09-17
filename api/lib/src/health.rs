b1911ebcb98737d472112501f6cde4fbe6601656
pascalbehmenburg <pascalbehmenburg@posteo.net>
2023-09-17 16:57:54 +0200

    added unit and integration tests for health endpoint

--- api/lib/src/health.rs
+++ api/lib/src/health.rs
@@ -1,12 +1,36 @@
 use actix_web::{HttpResponse, web};
 
+pub const API_VERSION: &str = "v0.0.1";
+
 async fn health() -> HttpResponse {
     tracing::info!("Returning health status");
     HttpResponse::Ok()
-        .append_header(("version", "0.0.1"))
+        .append_header(("version", API_VERSION))
         .finish()
 }
 
 pub fn service(cfg: &mut actix_web::web::ServiceConfig) {
     cfg.route("/health", web::get().to(health));
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::http::StatusCode;
+
+    use super::*;
+
+    #[actix_rt::test]
+    async fn health_check_works() {
+        let res = health().await;
+        
+        assert!(res.status().is_success());
+        assert_eq!(res.status(), StatusCode::OK);
+        
+        let data = res
+            .headers()
+            .get("version")
+            .and_then(|h| h.to_str().ok());
+
+        assert_eq!(data, Some(API_VERSION));
+    }
 }
