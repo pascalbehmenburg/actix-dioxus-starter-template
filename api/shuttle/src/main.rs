5099e861624d95300c21822eec97124653e12e0b
pascalbehmenburg <pascalbehmenburg@posteo.net>
2023-09-18 12:29:10 +0200

    inject film repository into actix data store

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,14 +1,16 @@
 use actix_web::web::ServiceConfig;
+use api_lib::todo_repository;
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[shuttle_runtime::main]
 async fn actix_web(
     #[shuttle_shared_db::Postgres()] pool: sqlx::PgPool,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
-    let pool = actix_web::web::Data::new(pool);
+    let todo_repository = todo_repository::PostgresTodoRepository::new(pool);
+    let todo_repository = actix_web::web::Data::new(todo_repository);
 
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.app_data(pool)
+        cfg.app_data(todo_repository)
             .configure(api_lib::health::service)
             .configure(api_lib::todos::service);
     };
